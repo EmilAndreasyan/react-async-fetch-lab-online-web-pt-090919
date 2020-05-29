e4272caa333acbc5aa9c04183a031752e4b062be
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-05-29 22:02:07 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import React from 'react';
 
 export default class App extends React.Component {
   this.state = {
-    
+    people: []
   }
   
   componentDidMount () {
-    fetch()
+    fetch('http://api.open-notify.org/astros.json')
+    .then(resp => resp.json())
+    .then(data => {
+      this.setState({
+        people: data
+      })
+    })
   }
 }
